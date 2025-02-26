cacc7263cc5ce11c17f67891c7eb897ca5159ccf
Yunkyeong Jang <yunkyeong@dencomm.ai>
2025-02-26 19:27:36 +0900

    [feat]: 사용자 페이지별로 조회하는 기능 추가 (QueryDsl 적용)

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,11 +18,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
-
-
     // mysql
     runtimeOnly("com.mysql:mysql-connector-j")
 
+    // querydsl
+    implementation ("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    annotationProcessor ("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    annotationProcessor ("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor ("jakarta.persistence:jakarta.persistence-api")
 
     // lombok
     implementation ("org.projectlombok:lombok")
@@ -35,6 +38,8 @@ dependencies {
     runtimeOnly("com.h2database:h2")
 
 }
+
+
 // 여기에 UTF-8 설정 추가
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
@@ -43,3 +48,21 @@ tasks.test {
     useJUnitPlatform()
 }
 
+/**
+ * QueryDSL Build Options
+ */
+val querydslDir = "${layout.projectDirectory}/build/generated/querydsl"
+
+sourceSets {
+    getByName("main").java.srcDirs(querydslDir)
+}
+
+tasks.withType<JavaCompile> {
+    options.generatedSourceOutputDirectory = file(querydslDir)
+}
+
+tasks.named("clean") {
+    doLast {
+        file(querydslDir).deleteRecursively()
+    }
+}
